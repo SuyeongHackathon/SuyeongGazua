87fb8f36fd136892b9fa7a66caf32902344e0ac3
Jsgithubchannel <jsschannel99@gmail.com>
2021-09-23 12:50:42 +0900

    Feat: add fontawesome, recommand-place in Main.js

--- ui/src/component/main/Main.js
+++ ui/src/component/main/Main.js
@@ -1,40 +1,43 @@
-import React from 'react';
-import './Main.css'
+import React from "react";
+import "./Main.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faInfoCircle } from "@fortawesome/free-solid-svg-icons";
 
 const Main = () => {
-    return (
-        <>
-        {/* 찐 */}
+  return (
+    <div className="container">
+      {/* 찐 */}
 
-        {/* 홍 */}
-            {/* 추천 관광지 */}
-          
-        <div className="recommand-place">
-            <div className="title-container">
-                <h1>추천 관광지</h1>
-                <p>Info 이모티콘 자리</p>
-                <button className="more-btn">더보기</button>
-            </div>
-            <div className="btn-container">
-                <div className="btn-row">
-                    <button>자연</button>
-                    <button>인문</button>
-                    <button className="long-btn">추천코스</button>
-                    <button>쇼핑</button>
-                    <button>음식</button>
-                    <button className="longest-btn">체험관광지</button>
-                </div>
-            </div>
-            <div className="detail">
-                <div className="row">
-                    <img src="" alt="" />
-                    <div className="title">콘텐츠명</div>
-                    <div className="content">제목</div>
-                </div>
-            </div>
+      {/* 홍 */}
+      {/* 추천 관광지 */}
+
+      <div className="recommand-place">
+        <div className="title-container">
+          <h1>추천 관광지</h1>
+          <FontAwesomeIcon icon={faInfoCircle} className="btn-info" />
+
+          <button className="btn-more">더보기</button>
         </div>
-        </>
-    );
+        <div className="btn-container">
+          <div className="btn-row">
+            <button>자연</button>
+            <button>인문</button>
+            <button className="btn-route">추천코스</button>
+            <button>쇼핑</button>
+            <button>음식</button>
+            <button className="btn-experience">체험관광지</button>
+          </div>
+        </div>
+        <div className="detail">
+          <div className="row">
+            <img src="" alt="" />
+            <div className="title">콘텐츠명</div>
+            <div className="content">제목</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Main;
